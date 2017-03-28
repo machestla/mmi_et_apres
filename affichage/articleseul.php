0cfc2b485257f9eaa1138ccddf2ea7c46732c94c
chloechagot <chloechagot@hotmail.fr>
2017-03-28 17:47:35 +0200

    rien de changé

--- affichage/articleseul.php
+++ affichage/articleseul.php
@@ -6,7 +6,6 @@ include("../divers/balises.php");
 
     <div class="container-fluid contenu-article">
         <?php
-
 if(isset($_GET['id'])){
   
    $sql = "SELECT * FROM article WHERE id=?";
@@ -17,22 +16,18 @@ if(isset($_GET['id'])){
          if(isset($line['contenu_video'])){
          echo' <h1 class="tittle-grey">'.$line['titre'].'</h1>
 <p class="date">'.$line['date'].'</p>
-
 <div class="contenu-article">
     <div class="row">
         <div class=" col-md-6">
          
          <video class="photo-video" width="400" height="222" controls="controls">
-        <source src="'.$line['contenu_video'].'" type="video/mp4" /> Ici l\'alternative à la vidéo : un lien de téléchargement, un message, etc.
+         <iframe width="560" height="315" src="'.$line['contenu_video'].'" frameborder="0" allowfullscreen></iframe>
+ Ici l\'alternative à la vidéo : un lien de téléchargement, un message, etc.
     </video>
      </div>
-
-
         <div class="texte-article col-md-6">
             <p>'.$line['contenu_txt'].'</p>
-
         </div>
-
     </div>
 </div>';
     }
@@ -40,34 +35,26 @@ if(isset($_GET['id'])){
          echo
            '<h1 class="tittle-grey">'.$line['titre'].'</h1>
 <p class="date">'.$line['date'].'</p>
-
 <div class="contenu-article">
     <div class="row">
         <div class=" pohto-video col-md-6">
             <img src="'.$line['contenu_visuel'].'">
         </div>
-
-
         <div class="texte-article col-md-6">
             <p>'.$line['contenu_txt'].'</p>
-
         </div>
-
     </div>
 </div>';
      }
-
     }
     
    
 }
-
   
    
 ?>
 
             <!--<div class="video">
-
     <video width="400" height="222" controls="controls">
         <source src="'.$line['contenu_video'].'" type="video/mp4" /> Ici l\'alternative à la vidéo : un lien de téléchargement, un message, etc.
     </video>-->
